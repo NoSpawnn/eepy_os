52a1808f23cb9d032c20186c38165d532ad4c057
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-09-13 23:52:34 +0100

    Fix 0xproto font path

--- build_files/install/fonts.sh
+++ build_files/install/fonts.sh
@@ -7,7 +7,7 @@ TEMP=$(mktemp -d)
 curl -fLs https://github.com/0xType/0xProto/releases/download/2.500/0xProto_2_500.zip -o $TEMP/0xproto.zip
 unzip $TEMP/0xproto.zip -d $TEMP
 
-mkdir /usr/share/0xproto
-cp $TEMP/*.ttf /usr/share/0xproto/
+mkdir /usr/share/fonts/0xproto
+cp $TEMP/*.ttf /usr/share/fonts/0xproto/
 
 rm -r $TEMP
