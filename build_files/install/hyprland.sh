473c3a996afb44b37a038e9db4fcea8b76e438c9
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-09-13 23:15:51 +0100

    Switch to newer copr repo for swaylock

--- build_files/install/hyprland.sh
+++ build_files/install/hyprland.sh
@@ -15,12 +15,10 @@ PACKAGES=(
     waybar
     fontawesome-fonts-all
     swaylock-effects
-    cascadia-code-nf-fonts
-    cascadia-mono-nf-fonts
 )
 
 # Enable copr for swaylock-effects
-dnf5 copr enable eddsalkield/swaylock-effects -y
+dnf5 copr enable trs-sod/swaylock-effects -y
 
 dnf5 install -y --allowerasing \
     --setopt=install_weak_deps=False \
