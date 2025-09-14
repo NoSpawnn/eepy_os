549753ac1d4bbfbf85a96811d3a29cafca5cde53
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-09-14 01:22:46 +0100

    Move font to correct location

--- build_files/install/fonts.sh
+++ build_files/install/fonts.sh
@@ -4,14 +4,15 @@
 set -euox pipefail
 
 TEMP=$(mktemp -d)
+FONT_DIR=/var/usrlocal/share/fonts/0xproto
 
 curl -fLs https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/0xProto.zip -o $TEMP/0xproto.zip
 unzip $TEMP/0xproto.zip -d $TEMP
 
-mkdir /usr/share/fonts/0xproto
-cp $TEMP/*.ttf /usr/share/fonts/0xproto/
-chown -R root: /usr/share/fonts/0xproto
-chmod 644 /usr/share/fonts/0xproto/*
-restorecon -vFr /usr/share/fonts/0xproto
+mkdir -p $FONT_DIR
+cp $TEMP/*.ttf $FONT_DIR/
+chown -R root: $FONT_DIR
+chmod 644 $FONT_DIR/*
+restorecon -vFr $FONT_DIR
 
 rm -r $TEMP
