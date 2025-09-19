d337121309f9cf532df231819d1d9285f0f5239e
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-09-20 00:54:42 +0100

    Fix copr syntax

--- build_files/1-system_config/coprs.sh
+++ build_files/1-system_config/coprs.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 COPRS_ENABLED=(
-    "bazzite-org:bazzite"
-    "bazzite-org:bazzite-multilib"
-    "ublue-os:staging"
+    "bazzite-org/bazzite"
+    "bazzite-org/bazzite-multilib"
+    "ublue-os/staging"
 )
 
 for copr in ${COPRS_ENABLED[@]}; do
     dnf5 -y copr enable "$copr"
+    dnf5 -y config-manager setopt copr:copr.fedorainfracloud.org:${copr////:}.priority=98 ;
 done
