306b6ffbccab2fe6f9522886fadc208a53e19faa
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-09-19 23:29:57 +0100

    Include fancontrol udev directly

--- build_files/system_config/udev_rules/fancontrol.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-curl -fLs --create-dirs https://raw.githubusercontent.com/wiiznokes/fan-control/master/res/linux/60-fan-control.rules \
-    -o /usr/lib/udev/rules.d/60-fan-control.rules
